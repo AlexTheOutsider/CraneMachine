7ad831cec84c6154f8ca085a579debe6beadf86c
Whalex <hsc19930818@gmail.com>
2018-11-25 00:37:49 -0500

    add score and spawn system

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    private bool hasBlock = true;
+    private float waitingTime = 0f;
+    public float spawnTime = 3f;
+    private int numberOfColliders = 0;
+
+    List<Collision2D> collidedObjects = new List<Collision2D>();
+    
+    //public List<GameObject> blockList;    
+    public GameObject[] blockListArray;
+
+    private void Start()
+    {
+        blockListArray = Resources.LoadAll<GameObject>("Prefabs");
+        //blockList = blockListArray.ToList();
+    }
+
+    private void Update()
+    {
+        print(numberOfColliders);
+        numberOfColliders = collidedObjects.Count;
+        if (numberOfColliders == 0)
+        {
+            waitingTime += Time.deltaTime;
+            if (waitingTime > spawnTime)
+            {
+                GameObject randomBlock = blockListArray[Random.Range(0, blockListArray.Length)];
+                Instantiate(randomBlock);
+                waitingTime = 0f;
+            }
+            collidedObjects.Clear();
+            return;
+        }
+        waitingTime = 0f;
+        collidedObjects.Clear();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!collidedObjects.Contains(other) && other.collider.tag == "Crate")
+        {
+            collidedObjects.Add(other);
+            //print("add");
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (!collidedObjects.Contains(other) && other.collider.tag == "Crate")
+        {
+            collidedObjects.Add(other);
+            //print("stay");
+        }
+    }
+}
---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    public GameController GameController;
+
+    public bool isHooked = false;
+    public bool isActivated = false;
+
+    void Update()
+    {
+/*		if (!isHooked)
+		{
+			GameController.ScoreUpdate(Mathf.RoundToInt(transform.position.y * 10f));
+		}*/
+        //print(isHooked);
+        //print(isActivated);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Crane"))
+        {
+            isHooked = true;
+        }
+
+        if (other.transform.CompareTag("Platform"))
+        {
+            isActivated = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Crane"))
+        {
+            isHooked = false;
+        }
+
+        if (other.transform.CompareTag("Platform"))
+        {
+            isActivated = false;
+        }
+    }
+}
